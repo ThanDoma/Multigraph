a43fe85b597a2434ffec16354cf0e33c46d812f7
ThanDoma <42151643+ThanDoma@users.noreply.github.com>
2025-05-27 16:24:34 +0300

    Delete ex.py

--- ex.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def get_points(radius, number_of_points):
-    radians_between_each_points = 2*np.pi/number_of_points
-    list_of_points = []
-    for p in range(0, number_of_points):
-        list_of_points.append( (radius*np.cos(p*radians_between_each_points), radius*np.sin(p*radians_between_each_points)) )
-
-    return list_of_points
